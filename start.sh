d2bd52a9398f847bfdfba6283f379c8c315dfa7b
Сергей <pinigin.s@ispsystem.com>
2018-07-22 05:26:06 +0800

    start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-LD_LIBRARY_PATH=pwd /root/chtobirabotalo/chtobirabotalo
-/root/chtobirabotalo/chtobirabotalo
+LD_LIBRARY_PATH=pwd nohup /root/chtobirabotalo/chtobirabotalo &
--- tests/vk_api_send_message.py
+++ tests/vk_api_send_message.py
@@ -3,4 +3,4 @@ import sys
 sys.path.append("../")
 from platforms.vk.client import VkClient
 
-VkClient().send_message(575780, "Ко мне постучался косматый геолог")
+VkClient().send_message(575780, "Я не шучу, сделай с этим уже что-нибудь.")
